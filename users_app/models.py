1db9cebb8b815be85839cf33db3ef9a97276e02d
AshMcCoy <77694729+AshMcCoy@users.noreply.github.com>
2021-03-30 07:04:03 -0500

    single_model_orm

---
+++ users_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+class User(models.Model):
+    first_name= models.CharField(max_length=255)
+    last_name= models.CharField(max_length=255)
+    email_address= models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __repr__(self):
+        return f"<User object: {self.first_name} {self.last_name}>"
+# Create your models here.
